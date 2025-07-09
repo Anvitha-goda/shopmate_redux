0619234891817bfdd7ed4aa6ef11912109dd1949
Anvitha-goda <anvitha.tammana@godainfotech.in>
2025-07-09 18:30:00 +0530

    redux concepts slice and action demo

---
+++ src/store/cartSlice.js
@@ -0,0 +1,21 @@
+// import { createSlice } from "@reduxjs/toolkit";
+const {createSlice} = require("@reduxjs/toolkit");
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        catrList: [],
+        total: 0
+    },
+    reducers: {
+        add(state,action){
+            
+        },
+        remove(state,action){
+
+        }
+    }
+});
+
+export const {add,remove} = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
